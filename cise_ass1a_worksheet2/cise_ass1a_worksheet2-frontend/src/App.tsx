fdb5ca9c08355b1626cd94969a9c263f1bd28d7c
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2023-07-30 20:18:55 +1200

    Completed worksheet 2

--- cise_ass1a_worksheet2/cise_ass1a_worksheet2-frontend/src/App.tsx
+++ cise_ass1a_worksheet2/cise_ass1a_worksheet2-frontend/src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Welcome to CISE - The home of learning and fun</h1>
-    </div>
-  );
+interface State {
+  count: number;
+}
+
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0,
+  };
+}
+
+makeIncrementer = (amount: number) => () =>
+  this.setState((prevState: State) => ({
+    count: prevState.count + amount,
+  }));
+
+  increment = this.makeIncrementer(1);
+
+  render(){
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+        Increment count
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
