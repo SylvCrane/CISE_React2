e6cd74f6d31a1464ba5e12f88187189fcc8c455a
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2023-08-12 23:03:16 +1200

    Update Links

--- cise_ass1a_worksheet3/my-app/src/components/ShowBookList.js
+++ cise_ass1a_worksheet3/my-app/src/components/ShowBookList.js
@@ -9,7 +9,7 @@ function ShowBookList() {
 
     useEffect(() => {
         axios
-            .get('http://localhost:8082/api/books')
+            .get('https://book-backend-1.vercel.app/api/books')
             .then((res) => {
                 setBooks(res.data);
             })
