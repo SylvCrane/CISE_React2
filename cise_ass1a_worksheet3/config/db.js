020c8d0683ad3823494f428ad8995de92a308353
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2023-08-12 22:33:24 +1200

    Update db.js

--- cise_ass1a_worksheet3/config/db.js
+++ cise_ass1a_worksheet3/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = "mongodb+srv://navjotnsandhu:gVIgeE8rx2lVndm4@bookservice.lownc3f.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async () => {
     try
